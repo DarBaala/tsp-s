912a1315b641caa8912ba2c2a8f3e103c8f1a8ec
DarBaala <joker.milan1997@gmail.com>
2023-03-30 15:31:23 +0300

    added slider

--- js/script.js
+++ js/script.js
@@ -140,4 +140,19 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
   }
+  const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
